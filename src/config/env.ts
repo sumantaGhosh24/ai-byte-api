a5fa8fc4a490adb29131f971b0426896663e159c
sumanta ghosh <84187999+sumantaGhosh24@users.noreply.github.com>
2026-05-10 12:08:17 +0530

    setup neon database with drizzle and typescript fix

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,4 +2,5 @@ import "dotenv/config";
 
 export const env = {
   PORT: process.env.PORT || 5000,
+  DATABASE_URL: process.env.DATABASE_URL!,
 };
---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import {drizzle} from "drizzle-orm/neon-http";
+import {neon} from "@neondatabase/serverless";
+
+import {env} from "../config/env";
+
+const sql = neon(env.DATABASE_URL);
+
+export const db = drizzle({client: sql});
